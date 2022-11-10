0a9d3b0505f4534d41c1826ed075bd5379f36164
Parker Anderson <parkersthinking@gmail.com>
2022-11-09 18:39:11 -0700

    Player Movement

---
+++ global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+const DEADZONE := 0.2
+
+var gravity := 75
--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,22 @@
 extends KinematicBody2D
 
+export(int) var controller_num = 0
+
 var velocity := Vector2(0, 0)
 var speed := 500
+var jump_speed := 1000
 
 func _physics_process(delta):
-	velocity.x = Input.get_joy_axis(0, JOY_AXIS_0)
+	var input_x := Input.get_joy_axis(controller_num, JOY_AXIS_0)
 	
-	move_and_slide(velocity * speed, Vector2.UP)
+	if abs(input_x) < Global.DEADZONE:
+		velocity.x = 0
+	else:
+		velocity.x = input_x * speed
+	
+	velocity.y += Global.gravity
+	
+	if Input.is_joy_button_pressed(controller_num, JOY_BUTTON_0) and is_on_floor():
+		velocity.y = -jump_speed
+	
+	velocity = move_and_slide(velocity, Vector2.UP)

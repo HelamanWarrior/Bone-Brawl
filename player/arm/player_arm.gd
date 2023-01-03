8de9de18084e8e2f444d911010ff31ffa58dd611
PlugWorld <40321938+HelamanWarrior@users.noreply.github.com>
2023-01-02 17:41:37 -0700

    Better arm spin and project organization

--- player/arm/player_arm.gd
+++ player/arm/player_arm.gd
@@ -4,9 +4,12 @@ var is_right_arm := false
 var fade_alpha := 1.0
 
 const FADE_BLUR_PARTICLE := preload("res://effects/fade_blur_particle/fade_blur_particle.tscn")
+const MINIMUM_SPIN_SPEED := 600
 
 onready var sprite := $Sprite
 onready var hitbox_collision := $Hitbox/CollisionShape2D
+onready var throw_animation_player := $ThrowAnimationPlayer
+onready var spin_complete_timer := $SpinCompleteTimer
 
 func _ready() -> void:
 	GameEvent.emit_signal("add_action_object", self)
@@ -14,25 +17,25 @@ func _ready() -> void:
 	hitbox_collision.disabled = true
 	apply_torque_impulse(1500)
 
+func _process(_delta: float) -> void:
+	if sleeping:
+		return
+	
+	if get_linear_velocity().length() < MINIMUM_SPIN_SPEED:
+		throw_animation_player.playback_speed = lerp(throw_animation_player.playback_speed, 0, 0.1)
+		spin_complete_timer.wait_time = 0.8 * throw_animation_player.playback_speed
+	else:
+		throw_animation_player.play("throw")
+
+func _on_SpinCompleteTimer_timeout() -> void:
+	if throw_animation_player.playback_speed < 0.5:
+		sprite.frame = 0
+		throw_animation_player.stop()
+
 func _on_DeactivateHitboxTimer_timeout() -> void:
 	# really crappy way of doing this
 	# fix later, will lead to issues
 	hitbox_collision.disabled = false
-
-func instance_fade() -> void:
-	fade_alpha *= 0.8
-	var particle_instance := FADE_BLUR_PARTICLE.instance()
-	
-	particle_instance.texture = sprite.texture
-	particle_instance.hframes = sprite.hframes
-	particle_instance.frame = sprite.frame
-	particle_instance.flip_h = sprite.flip_h
-	particle_instance.scale = sprite.scale
-	particle_instance.rotation = sprite.rotation
-	particle_instance.global_position = global_position
-	particle_instance.modulate.a = fade_alpha
-	
-	get_tree().current_scene.add_child(particle_instance)
 
 func _exit_tree() -> void:
 	GameEvent.emit_signal("remove_action_object", self)
